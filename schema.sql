7183836fdc1263d35dd21db542f04629e6b6a067
Ben Snyder <BenSnyder90@gmail.com>
2020-04-25 13:46:54 -0400

    Updated table names to all lowercase for postgres purposes. Updated ER Diagram

--- schema.sql
+++ schema.sql
@@ -1,37 +1,34 @@
-DROP TABLE US_State_Pop IF EXISTS;
-DROP TABLE Daily_COVID_Stats IF EXISTS;
-DROP TABLE Final_COVID_Stats IF EXISTS;
+DROP TABLE us_state_pop CASCADE;
+DROP TABLE daily_covid_stats CASCADE;
+DROP TABLE final_covid_stats;
 
 -- Create Three Tables
-CREATE TABLE US_State_Pop (
+CREATE TABLE us_state_pop (
   state varchar(2) PRIMARY KEY,
   name varchar,
-  2019_pop INT
+  pop_2019 INT
 );
 
-CREATE TABLE Daily_COVID_Stats (
-  date varchar PRIMARY KEY,
-  state varchar(2) PRIMARY KEY,
+CREATE TABLE daily_covid_stats (
+  date varchar,
+  state varchar(2),
   positive_tests INT,
   total_tests INT,
   deaths INT,
-  FOREIGN KEY (state) REFERENCES US_State_Pop(state)
+  PRIMARY KEY (date, state),
+  FOREIGN KEY (state) REFERENCES us_state_pop(state)
 );
 
-CREATE TABLE Final_COVID_Stats (
-  date varchar PRIMARY KEY,
-  state varchar(2) PRIMARY KEY,
+CREATE TABLE final_covid_stats (
+  date varchar,
+  state varchar(2),
+  name varchar,
   positive_tests INT,
   death INT,
   total_tests INT,
-  2019_pop INT,
+  pop_2019 INT,
   pct_tested FLOAT,
-  tested_per_million FLOAT
-  FOREIGN KEY (date) REFERENCES Daily_COVID_Stats(date),
-  FOREIGN KEY (state) REFERENCES Daily_COVID_Stats(state),
-  FOREIGN KEY (positive_tests) REFERENCES Daily_COVID_Stats(positive_tests),
-  FOREIGN KEY (total_tests) REFERENCES Daily_COVID_Stats(total_tests),
-  FOREIGN KEY (death) REFERENCES Daily_COVID_Stats(death),
-  FOREIGN KEY (name) REFERENCES US_State_Pop(name),
-  FOREIGN KEY (2019_pop) REFERENCES US_State_Pop(2019_pop)
+  tested_per_million FLOAT,
+  PRIMARY KEY (date, state),
+  FOREIGN KEY (state) REFERENCES us_state_pop(state)
 );
